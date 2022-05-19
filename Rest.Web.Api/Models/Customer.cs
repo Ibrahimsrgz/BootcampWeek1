1ed9b71c867d3971b36a1c1c6d0a53efa42d6287
ibrahim sarıgöz <srgzibrahim@gmail.com>
2022-05-19 18:55:39 +0300

    I  produced customer class with its properties.

--- Rest.Web.Api/Models/Customer.cs
+++ Rest.Web.Api/Models/Customer.cs
@@ -3,6 +3,11 @@
     public class Customer
     {
 
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Mail { get; set; }
+        public int Telephone { get; set; }
 
 
     }
