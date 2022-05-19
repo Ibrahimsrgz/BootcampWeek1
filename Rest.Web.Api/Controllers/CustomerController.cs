7a2d6ef47b105c19b3d2aea9afb9cb0f3d88c7f6
ibrahim sarıgöz <srgzibrahim@gmail.com>
2022-05-19 22:41:32 +0300

    Static list  created.

--- Rest.Web.Api/Controllers/CustomerController.cs
+++ Rest.Web.Api/Controllers/CustomerController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Rest.Web.Api.Models;
+using System.Collections.Generic;
 
 namespace Rest.Web.Api.Controllers
 {
@@ -7,6 +9,36 @@ namespace Rest.Web.Api.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
+        public static List<Customer> customerList = new List<Customer>()
+        {
+            new Customer
+            {
+                CustomerId =1,
+                Address = "Izmir",
+                Name = "Ibrahim",
+                Mail ="ibrahim@gmail.com",
+                Telephone = "65754643543"
+            },
+            new Customer
+            {
+                CustomerId =2,
+                Name = "Aylis",
+                Address = "Güzelyurt",
+                Mail ="aylis@gmail.com",
+                Telephone = "5323013145"
+            },
+            new Customer
+            {
+                CustomerId =3,
+                Name = "Ismail",
+                Address = "Ankara",
+                Mail ="ismail@gmail.com",
+                Telephone = "532306545"
+            }
+        };
+
+
+
 
     }
 }
